275aa5de4ad8ebce72d7c8db84293eb5c267d13f
Brahim1995 <you@example.com>
2023-07-06 03:27:44 +0100

    jawek behi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Container,Navbar } from 'react-bootstrap';
+import ListPlayer from './components/ListPlayer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className='play'>
+        <Container>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#">****FIFA Players****</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </Container>
+          <ListPlayer/>
+          <Container>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#">****Best FIFA Players Ever****</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </Container>
+      </div>
+
   );
 }
 
 export default App;
+
+
